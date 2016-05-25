ccff5f72921e6cbcb453b8103d5816ec55d8169f
David Fahlander <david.fahlander@gmail.com>
2016-05-26 00:51:15 +0200

    API skelleton

---
+++ yieldreq.js
@@ -0,0 +1,24 @@
+
+(function (global, factory) {
+   typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
+   typeof define === 'function' && define.amd ? define(factory) :
+   global.yieldreq = factory();
+}(this, function () { 'use strict';
+    return {
+        await: function (iterator) {
+            
+        },
+        async: function (fn) {
+            
+        },
+        spawn: function (fn) {
+            
+        },
+        all: function (array) {
+            
+        },
+        bulk: function (array) {
+            
+        }
+    };
+}));
